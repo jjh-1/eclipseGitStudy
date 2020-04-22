851065da49fbaa29714a884c44a3bfd49521e0c8
jjh <jhj9490@gmail.com>
2020-04-22 13:27:16 +0900

    this is added in local

--- p2/src/p2/pushPrint.java
+++ p2/src/p2/pushPrint.java
@@ -5,7 +5,7 @@ public class pushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add new class");
-
+		System.out.println("this is added in local");
 	}
 
 }
